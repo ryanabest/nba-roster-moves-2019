d67b196ff6dbcddeb271b0361b80e6833ed90f6d
ryanabest <ryanabest@gmail.com>
2019-02-08 14:57:08 -0500

    Add all rosters file

--- combineTransactions.js
+++ combineTransactions.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
-let allTransactions = []
+let allTransactions = [];
+let allRosters = [];
 
 fs.readdir('data/Transactions/', function(err,files) {
   files.forEach(function(file,index) {
@@ -12,4 +13,17 @@ fs.readdir('data/Transactions/', function(err,files) {
 setTimeout(function() {
   // console.log(allTransactions);
   fs.writeFileSync('data/AllTransactions/AllTransactions.json',JSON.stringify(allTransactions));
+},5000);
+
+fs.readdir('data/OpeningDayRosters/', function(err,files) {
+  files.forEach(function(file,index) {
+    // console.log(file);
+    let roster = JSON.parse(fs.readFileSync('data/OpeningDayRosters/'+file, 'utf-8'));
+    allRosters.push(roster[0]);
+  });
+});
+
+setTimeout(function() {
+  // console.log(allTransactions);
+  fs.writeFileSync('data/AllRosters/AllRosters.json',JSON.stringify(allRosters));
 },5000);
